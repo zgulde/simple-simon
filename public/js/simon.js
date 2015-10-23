4e4472fde91464f0da98d8f409ac801086d61e24
Zach <zach@Zachs-MacBook-Pro.local>
2015-10-23 11:38:38 -0500

    added audio functionality

--- public/js/simon.js
+++ public/js/simon.js
@@ -4,13 +4,30 @@
 	var colorsLit = [];
 	var colorsIndex = 0;
 	var lightUpSpeed = 350;
-	var blueSound = new Audio('/media/A2.wav');
-	var yellowSound = new Audio('/media/csharp3.wav');
-	var redSound = new Audio('/media/E3.wav');
-	var greenSound = new Audio('/media/A3.wav');
+	var notes = {
+		a2: new Audio('/media/A2.wav'),
+		cs3: new Audio('/media/CS3.wav'),
+		e3: new Audio('/media/E3.wav'),
+		a3: new Audio('/media/A3.wav'),
+		c3: new Audio('/media/C3.wav')
+	}
+	var blueSound = notes.a2;
+	var yellowSound = notes.cs3;
+	var redSound = notes.e3;
+	var greenSound = notes.a3;
+	var lossSound = notes.c3;
 
 	function randomNumber(min,max){
 		return Math.floor( (Math.random() * (max-min+1) + min) );
+	}
+
+	function playThenPause (sound,duration) {
+		if(!duration) duration = 300;
+		sound.play();
+		setTimeout( function(){
+			sound.pause();
+			sound.load();
+		}, duration);
 	}
 
 	function getSound (color) {
@@ -31,6 +48,8 @@
 	}
 
 	function lightUp($color){
+		var sound = getSound($color.attr('id'));
+		playThenPause(sound);
 		$color.addClass('light-up');
 		$color.addClass($color.attr('id') + '-lit');
 		setTimeout( function(){
@@ -69,7 +88,7 @@
 
 	function startNewRound () {
 		colorsIndex = 0;
-		$('.color-btn').off('click');
+		$('.color-btn').off('click',checkClicks);
 		addRandomColor();
 		$('#middle-btn-text').text('Round ' + colorsLit.length);
 		showCurrentSequence();
@@ -88,8 +107,13 @@
 			$('#middle-btn-text').text('Again?');
 			$('#middle-btn').on('click',startGame);
 		}, 2000);
+		playThenPause(lossSound,1000);
 	}
 
 	$('#middle-btn').click(startGame);
+	$('.color-btn').click(function(){
+		var sound = getSound($(this).attr('id'));
+		playThenPause(sound,200);
+	})
 
 // });
